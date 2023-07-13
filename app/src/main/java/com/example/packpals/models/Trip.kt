27bfa0d9567240d4dc3a66472392f9df68a056cd
Michelle Wong <yy8wong@uwaterloo.ca>
2023-07-13 01:13:45 -0400

    added minor fixes to trips UI

--- app/src/main/java/com/example/packpals/models/Trip.kt
+++ app/src/main/java/com/example/packpals/models/Trip.kt
@@ -1,10 +1,8 @@
 package com.example.packpals.models
 
-import java.util.Date
-
 data class Trip(
     val title: String? = null,
     val tripCreatorId: String? = null,
     val tripPalIds: List<String>? = null,
-    val status: String? = "upcoming", //default to upcoming
+    val active: Boolean? = true //default to active after creation
 )
