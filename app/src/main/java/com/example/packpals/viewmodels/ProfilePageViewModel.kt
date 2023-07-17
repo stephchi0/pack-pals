c9d510f29452e89267228cd3ef49d67005cdf95c
Stephen Chi <stephen_chi@hotmail.ca>
2023-07-17 03:26:00 -0400

    deleted extra files

--- app/src/main/java/com/example/packpals/viewmodels/ProfilePageViewModel.kt
+++ app/src/main/java/com/example/packpals/viewmodels/ProfilePageViewModel.kt
@@ -7,14 +7,10 @@ import androidx.lifecycle.viewModelScope
 import com.example.packpals.models.Pal
 import com.example.packpals.repositories.AuthRepository
 import com.example.packpals.repositories.PalsRepository
-import com.example.packpals.repositories.ProfileRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Date
 import javax.inject.Inject
 import android.util.Log
-import kotlin.math.round
 
 @HiltViewModel
 class ProfilePageViewModel @Inject constructor(
