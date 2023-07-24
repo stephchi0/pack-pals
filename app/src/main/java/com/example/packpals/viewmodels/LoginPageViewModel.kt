9e0eab2ec7172016fdecbfe3a95366fca3472a69
Joey Chik <joey.chik0103@gmail.com>
2023-07-24 17:23:56 -0400

    loginpageviewmodel changes

--- app/src/main/java/com/example/packpals/viewmodels/LoginPageViewModel.kt
+++ app/src/main/java/com/example/packpals/viewmodels/LoginPageViewModel.kt
@@ -62,16 +62,7 @@ class LoginPageViewModel @Inject constructor(private val authRepo: AuthRepositor
                         profilePictureURL = storageRepo.uploadProfilePicture(newUserId, profilePictureUri).toString()
                     }
 
-                    var newPal = Pal(
-                        id = newUserId,
-                        name = name,
-                        email = null,
-                        profilePictureURL = profilePictureURL,
-                        pals = listOf(),
-                        palRequests = listOf(),
-                        gender = null,
-                        age = null,
-                        bio = null)
+                    var newPal = Pal(newUserId, name, profilePictureURL, listOf(), listOf(), null, null, null)
                     palsRepo.createPal(newPal)
 
                     _registrationSuccess.value = true
